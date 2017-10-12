9497dfa1e6ef0fb254c5c08a5d4958615bc26379
Cichar <363655056@qq.com>
2017-10-12 21:13:52 +0800

    make the server class work

--- Server/server.py
+++ Server/server.py
@@ -2,12 +2,20 @@
 
 
 class Server(object):
-    def __init__(self, host: str='127.0.0.1', port: int=8888):
-        self.host = host
-        self.port = port
+    def __init__(self, app, host: str='127.0.0.1', port: int=8555):
+        self._host = host
+        self._port = port
+        self.application = app
 
     def start(self):
-        pass
+        from wsgiref.simple_server import make_server
+
+        try:
+            server = make_server(self._host, self._port, self.application)
+            print('Sloth on http://{0}:{1}'.format(self._host, self._port))
+            server.serve_forever()
+        except KeyboardInterrupt:
+            pass
 
     def __repr__(self):
-        return '{0} ({1}:{2})'.format(self.__class__.__name__, self.host, self.port)
+        return '{0} ({1}:{2})'.format(self.__class__.__name__, self._host, self._port)
