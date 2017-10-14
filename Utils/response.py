cec1f448a2c58edf9c3c5a47846f1ff771787bb5
Cichar <363655056@qq.com>
2017-10-14 11:11:49 +0800

    verify response.py

--- Utils/response.py
+++ Utils/response.py
@@ -4,4 +4,26 @@ from .baseobject import ThreadSafeObject
 
 
 class SlothResponse(ThreadSafeObject):
-    pass
+    def __init__(self):
+        self.__status = '200 '
+        self.__headers = [('Content-type', 'text/plain')]
+
+        self.__body = None
+
+    def set_body(self, body):
+        self.__body = body().encode()
+
+    def set_status(self, status: int):
+        self.__status = '%s ' % status
+
+    @property
+    def status(self):
+        return self.__status
+
+    @property
+    def headers(self):
+        return self.__headers
+
+    @property
+    def body(self):
+        return self.__body
