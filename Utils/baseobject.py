bc57d427769fc20e2b6936c51890ee3a0cb381ac
Cichar <363655056@qq.com>
2017-10-14 11:12:05 +0800

    add singleton class

--- Utils/baseobject.py
+++ Utils/baseobject.py
@@ -3,6 +3,17 @@
 from threading import local
 
 
+class Singleton(type):
+    def __init__(cls, *args, **kwargs):
+        super(Singleton, cls).__init__(*args, **kwargs)
+        cls._instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instance
+
+
 class SlothBaseException(Exception):
     """
         Provide a base exception object for Sloth
