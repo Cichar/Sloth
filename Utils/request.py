c5bfeabc5e911ba459bce050fa8348089b6029b9
Cichar <363655056@qq.com>
2017-10-14 11:11:56 +0800

    verify request.py

--- Utils/request.py
+++ Utils/request.py
@@ -4,8 +4,17 @@ from .baseobject import ThreadSafeObject
 
 
 class SlothRequest(ThreadSafeObject):
-    def __init__(self):
-        self.__environ = None
-
-    def bind_environ(self, environ):
+    def __init__(self, environ):
         self.__environ = environ
+
+    @property
+    def path(self):
+        return self.__environ.get('PATH_INFO', None)
+
+    @property
+    def method(self):
+        return self.__environ.get('REQUEST_METHOD', 'GET')
+
+    @property
+    def query_string(self):
+        return self.__environ.get('QUERY_STRING', '')
