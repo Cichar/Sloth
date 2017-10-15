a2db11eac4012e77e05a829e321312c0f2205db2
Cichar <363655056@qq.com>
2017-10-15 19:54:53 +0800

    add HTTPError class.

--- Utils/exception.py
+++ Utils/exception.py
@@ -1,7 +1,23 @@
 # -*- coding:utf-8 -*-
 
+from .objects import normal_response
 from .baseobject import SlothBaseException
 
 
 class HandlerException(SlothBaseException):
     pass
+
+
+class HTTPError(SlothBaseException):
+    def __init__(self, status_code: int, message: str = None, *args, **kwargs):
+        self.status_code = status_code
+        self.message = None
+        self.reason = kwargs.get('reason', None)
+
+    def __str__(self):
+        message = "HTTP %d: %s" % (self.status_code,
+                                   self.reason or normal_response[self.status_code])
+        if self.message:
+            return message + "(" + self.message + ")"
+        else:
+            return message
