e278ac200a795fd6a7cdd672428f1cf1fc110368
Cichar <363655056@qq.com>
2017-10-16 09:54:18 +0800

    create router and rigister exceptions

--- Utils/exception.py
+++ Utils/exception.py
@@ -8,10 +8,22 @@ class HandlerException(SlothBaseException):
     pass
 
 
+class RouterException(SlothBaseException):
+    pass
+
+
+class RouterTypeException(SlothBaseException):
+    pass
+
+
+class RegisterException(SlothBaseException):
+    pass
+
+
 class HTTPError(SlothBaseException):
     def __init__(self, status_code: int, message: str = None, *args, **kwargs):
         self.status_code = status_code
-        self.message = None
+        self.message = message
         self.reason = kwargs.get('reason', None)
 
     def __str__(self):
