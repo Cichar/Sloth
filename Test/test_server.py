f4e19958d88979f5d3d3e7017ad76a15097f5c00
Cichar <363655056@qq.com>
2017-10-23 16:12:17 +0800

    verify code

--- Test/test_server.py
+++ Test/test_server.py
@@ -1,16 +1,19 @@
 # -*- coding:utf-8 -*-
 
 from Sloth.Application import Sloth
+from Sloth.Utils.objects import Router
 from Sloth.Utils.response import SlothResponse
 
 from Sloth.Server import Server
 
 
 class IndexHandler(SlothResponse):
-    pass
+    def get(self, *args, **kwargs):
+        self.render_string('Hello Kitty!')
 
 
-app = Sloth(routers={'/': IndexHandler})
+routers = [Router(path='/hello', response=IndexHandler)]
+app = Sloth(routers=routers)
 server = Server(app)
 
 if __name__ == '__main__':
