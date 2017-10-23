8336159d2c60d7ca63aa2d86413d9444e6dcf212
Cichar <363655056@qq.com>
2017-10-23 16:10:51 +0800

    add Router Object to register Router

--- Sloth/Utils/objects.py
+++ Sloth/Utils/objects.py
@@ -3,3 +3,21 @@
 from http import HTTPStatus
 
 normal_response = {v: v.phrase for v in HTTPStatus.__members__.values()}
+
+
+class Router(object):
+    """ Object for register router
+        Example:
+            class IndexHandler(SlothResponse):
+                def get(self, *args, **kwargs):
+                    self.render_string('Hello World!')
+
+            routers = [Router(path='/hello', response=IndexHandler)]
+    """
+
+    def __init__(self, path: str, response: object, name: str=None):
+        if not isinstance(path, str) or isinstance(name, str):
+            raise TypeError('Path must be string type, get %s' % type(path))
+        self.path = path
+        self.response = response
+        self.name = name
